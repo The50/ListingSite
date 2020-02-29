40fe984ccd4f9b5d2218a77ca9c0990ab6c9745a
Erikas <e50erikas@gmail.com>
2020-02-29 14:22:39 +0200

    composer update

--- src/Repository/ListingRepository.php
+++ src/Repository/ListingRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Listing;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Collections\Collection;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Listing|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ListingRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct(
             $registry,
